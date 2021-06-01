dcd2070637dd240673dac1ff6e9d6df6f6347c47
hnana00 <kkokko5080@naver.com>
2021-06-01 21:59:15 +0900

    ~~~

--- src/com/hanna/basic1/MaximumValueOfThreeValues.java
+++ src/com/hanna/basic1/MaximumValueOfThreeValues.java
@@ -3,6 +3,6 @@ package com.hanna.basic1;
 public class MaximumValueOfThreeValues {
 
 	public static void main(String[] args) {
-		System.out.println("세 값의 최대값 구하기.");
+		System.out.println("Maximum value of three values");
 	}
 }
