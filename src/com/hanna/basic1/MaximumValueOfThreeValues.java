80d5c8ac5bf625b6e966ca85d3e8c5e39dd7d6ea
hanna <kkokko5080@naver.com>
2021-06-01 21:39:11 +0900

    hi

---
+++ src/com/hanna/basic1/MaximumValueOfThreeValues.java
@@ -0,0 +1,8 @@
+package com.hanna.basic1;
+
+public class MaximumValueOfThreeValues {
+
+	public static void main(String[] args) {
+		System.out.println("세 값의 최대값 구하기.");
+	}
+}
---
+++ src/com/hanna/test/test.java
@@ -0,0 +1,7 @@
+package com.hanna.test;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println("===== TEST ======");
+	}
+}
