88cde1eb2031f67c04adefb274992e6d2f311f7c
hnana00 <85140419+hnana00@users.noreply.github.com>
2021-07-29 15:18:45 +0900

    .

--- src/com/hanna/basic605/QuickSort.java
+++ src/com/hanna/basic605/QuickSort.java
@@ -12,12 +12,60 @@ public class QuickSort {
 		a[idx2] = t;
 	}
 	
+	//피벗 선택 1방법 = 임의로 요소 3개 선택 후 중앙값을 구하여 반환
+	static int med3( int a, int b, int c ) {
+		
+			if ( a >= b )
+				if ( b >= c )
+					return b;
+		
+				else if ( a <= c )
+					return a;
+		 
+				else
+					return c;
+		
+			else if ( a > c )
+				return a;
+		
+			else if ( b > c )
+				return c;
+		
+			else
+				return b;
+		}
+	
+	// 피벗 선택 2방법 = x[a], x[b], x[c]를 sort (중앙값의 index를 반환)
+		static int sort3Elem( int[] x, int a, int b, int c ) {
+			
+			if ( x[b] < x[a] )
+				swap( x, b, a );
+			
+			if ( x[c] < x[b] )
+				swap( x, c, b );
+			
+			if ( x[b] < x[a] )
+				swap( x, b, a );
+			
+			return b;
+		}
+		
+	//재귀
 	static void quicksort( int[] a, int left, int right ) {
 
 		int pl = left;
 		int pr = right;
 		int x = a[(pl + pr) / 2];
 		
+		/* 
+		 	--- 피벗 선택 2방법 ---
+		    int m = sort3Elem( a, pl, (pl + pr) / 2, pr );
+			x = a[m]; //피벗
+			swap(a, m, right - 1); //가운데 요소와 끝에서 두 번째  요소를 교환
+			pl++;  //왼쪽 시작위치
+			pr--;  //오른쪽 시작위치
+		*/
+
 		System.out.printf("a[%d]~[%d] : {",left,right);
 		
 		for( int i = left; i < right; i++ )
@@ -37,6 +85,20 @@ public class QuickSort {
 			
 		} while( pl <= pr );
 		
+		//요소 개수가 적은 그룹 먼저 나누는 메서드
+		if ( pr - left < right - pl ) {
+			
+			int temp;
+			
+			temp = left;
+			left = pl;
+			pl = temp;
+			
+			temp = right;
+			right = pr;
+			pr = temp;
+		}
+
 		if( left < pr ) {
 			quicksort( a, left, pr );
 		}
--- src/com/hanna/basic605/QuickSort2.java
+++ src/com/hanna/basic605/QuickSort2.java
@@ -13,6 +13,22 @@ public class QuickSort2 {
 		a[idx2] = t;
 	}
 
+	// 요소 개수가 9개 이하 일때 단순 삽입 정렬
+	static void insertionSort( int[] a, int left, int right ) {
+			
+		for ( int i = left + 1; i <= right; i++ ) {
+				
+			int tmp = a[i];
+			int j;
+				
+			for ( j = i; j > left && a[j - 1] > tmp; j-- )
+				a[j] = a[j - 1];
+				
+			a[j] = tmp;
+		}
+	}
+		
+	//비재귀
 	static void quickSort( int[] a, int left, int right ) {
 		
 		IntStack lstack = new IntStack( right - left + 1 ); //스택 생성
@@ -40,11 +56,27 @@ public class QuickSort2 {
 				
 			} while( pl <= pr );
 			
+			//요소 개수가 적은 그룹 먼저 나누는 메서드
+			if ( pr - left < right - pl ) {
+				
+				int temp;
+				
+				temp = left;
+				left = pl;
+				pl = temp;
+				
+				temp = right;
+				right = pr;
+				pr = temp;
+			}
+			
 			if( left < pr ) {
 				
 				lstack.push(left);
 				rstack.push(pr);
-				System.out.printf("a[%d]~a[%d]를 분할하는 문제를 스택에 푸시합니다.\n", left, pr); //푸시 팝 분할과정
+				
+				//푸시 팝 분할과정
+				System.out.printf("a[%d]~a[%d]를 분할하는 문제를 스택에 푸시합니다.\n", left, pr);
 				System.out.printf("Lstack:\n");
 				lstack.dump();
 				System.out.printf("Rstack:\n");
@@ -56,7 +88,9 @@ public class QuickSort2 {
 				
 				lstack.push(pl);
 				rstack.push(right);
-				System.out.printf("a[%d]~a[%d]를 분할하는 문제를 스택에 푸시합니다.\n", pl, right); //푸시 팝 분할과정
+				
+				//푸시 팝 분할과정
+				System.out.printf("a[%d]~a[%d]를 분할하는 문제를 스택에 푸시합니다.\n", pl, right);
 				System.out.printf("Lstack:\n");
 				lstack.dump();
 				System.out.printf("Rstack:\n");
